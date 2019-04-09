88792322a85c53b5a65fed9fbf3cd19aae64e2cb
guo feng <guofeng20094@gmail.com>
2019-04-10 00:42:30 +0800

    loaded xml data but config failed

--- src/main/java/orz/nurseroastering/controller/ApiController.java
+++ src/main/java/orz/nurseroastering/controller/ApiController.java
@@ -1,14 +1,34 @@
 package orz.nurseroastering.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import orz.nurseroastering.domain.ShiftAssignment;
+import orz.nurseroastering.service.ShiftService;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class ApiController {
 
+    @Autowired
+    private ShiftService shiftService;
+
     @RequestMapping("/hello")
     public String index() {
         return "API worked!";
+    }
+
+    @RequestMapping("/start")
+    public List<ShiftAssignment> startPlan() {
+        List<ShiftAssignment> plan;
+        try {
+            plan = shiftService.plan();
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
+        return plan;
     }
 
 }
